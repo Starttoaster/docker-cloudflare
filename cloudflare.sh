0675c7d36b06f24b78ba0371914704fa4bef48a8
Brandon <bmbawb@gmail.com>
2019-05-17 15:52:26 +0000

    Added support for proxied subdomains

--- cloudflare.sh
+++ cloudflare.sh
@@ -39,22 +39,33 @@ getSub() {
                 | sed 's/"//g' \
                 | cut -f1 -d"."
 }
+getProxy() {
+	wget --no-check-certificate --header="X-Auth-Email: "$EMAIL"" --header="X-Auth-Key: "$GAPIK"" \
+		-qO- "https://api.cloudflare.com/client/v4/zones/"$ZONE"/dns_records/"$IDENT"" \
+		| tr '[' '\n' \
+		| tr ',' '\n' \
+		| sed 's/{//g' \
+		| grep "proxied" \
+		| sed 's/"proxied"://g' \
+		| sed 's/"//g'
+}
 setIP() {
 	IP=$(curl -s4 ifconfig.co)
 	curl -sX PUT "https://api.cloudflare.com/client/v4/zones/"$ZONE"/dns_records/"$IDENT"" \
 		-H "X-Auth-Email: "$EMAIL"" \
 		-H "X-Auth-Key: "$GAPIK"" \
 		-H "Content-Type: application/json" \
-		--data '{"type":"A","name":"'$SUB'","content":"'$IP'"}' \
+		--data '{"type":"A","name":"'$SUB'","content":"'$IP'","proxied":'$PROXY'}' \
 		| tr '[' '\n' \
 		| tr ',' '\n' \
-		| grep "content\|name\|success" \
+		| grep "content\|name\|proxied\|success" \
 		| grep -v "zone" \
 		| sed 's/ //g'
 }
 getIdent | while read -r IDENT; do
         if [ "$(getType)" = "A" ]; then
                 SUB=$(getSub)
+		PROXY=$(getProxy)
                 setIP
         fi;done
 sleep "$INTERVAL"s
